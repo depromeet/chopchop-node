79e82ce3ab64834244c317af2878857de9158978
Jjin <wowlsyou@naver.com>
2017-07-09 17:36:20 +0900

    modify routes/review_comment models/review

--- models/review.js
+++ models/review.js
@@ -18,8 +18,7 @@ module.exports = function(sequelize, DataTypes) {
     review_img      : { type : DataTypes.STRING(20) },
     review_like     : { type : DataTypes.INTEGER },
     review_bad      : { type : DataTypes.INTEGER },
-    review_report   : { type : DataTypes.INTEGER },
-    review_comment  : { type : DataTypes.STRING(50) },
+    review_report   : { type : DataTypes.INTEGER }
   },
   {
 	  classMethods: {},
--- routes/review_comment.js
+++ routes/review_comment.js
@@ -37,19 +37,19 @@ router.get('/', function(req, res) {
   });
 });
 
-// 댓글 조회
-router.get('/:idx', function(req, res) {
-  var result = {};
-  var review_id = req.params.idx;
-
-  models.Review_Comment.findById(review_id)
-  .then(function(review) {
-    if (!review) res.status(400).send('not found');
-    res.status(200).json(review);
-  }).catch(function(err) {
-    res.status(500).send(err.message);
-  });
-});
+// // 댓글 조회
+// router.get('/:idx', function(req, res) {
+//   var result = {};
+//   var review_id = req.params.idx;
+//
+//   models.Review_Comment.findById(review_id)
+//   .then(function(review) {
+//     if (!review) res.status(400).send('not found');
+//     res.status(200).json(review);
+//   }).catch(function(err) {
+//     res.status(500).send(err.message);
+//   });
+// });
 
 // 댓글 수정
 router.put('/:idx', modifyComment);
@@ -64,31 +64,17 @@ function regisComment(req, res){
             rc_id  : null,
             status : null,
             reason : null
-        },
-
-        value = {
-            review_comment : 0
-        },
-
-        reviewIdMatched = {
-            review_id: commentInfo.rc_reviewid
         };
 
     models.Review_Comment.create(commentInfo).then(function(ret){
-        models.Review.findAll({where: reviewIdMatched}).then(function(comment){
-            value.review_comment = comment[0].dataValues.review_comment + 1;
-
-            models.Review.update(value, {where: reviewIdMatched}).then(function () {
-                result.rc_id = ret.rc_id;
-                result.status = 'S';
-                res.status(200).json(result);
-            }, function (err) {
-                result.status = 'F';
-                result.reason = err;
-                res.status(400).json(result);
-            })
-        })
-    });
+        result.rc_id = ret.rc_id;
+        result.status = 'S';
+        res.status(200).json(result);
+    }, function (err) {
+        result.status = 'F';
+        result.reason = err;
+        res.status(400).json(result);
+    })        
 }
 
 // 댓글 수정 params로 댓글 아이디 입력받음
@@ -100,7 +86,6 @@ function modifyComment(req, res) {
     status: null,
     reason: null
   };
-
   models.Review_Comment.update(rcinfo, {
     where: {
       rc_id: rc_id
@@ -116,45 +101,29 @@ function modifyComment(req, res) {
   });
 }
 
-// 댓글 삭제, params로 rc_id 받음
+// 댓글 삭제 ,params로 rc_id 받음
 function deleteComment(req, res){
-    var rc_id = req.params.idx,
-        result = {
-            status : null,
-            reason : null
-        },
-
-        value = {
-            review_comment : 0
-        },
-
-        review_id = 0;
-
-    models.Review_Comment.findById(rc_id).then(function(comment){
-        review_id = comment.dataValues.rc_reviewid;
-        models.Review.findById(review_id).then(function(review){
-            value.review_comment = review.dataValues.review_comment - 1;
-            models.Review.update(value,{where:{review_id: review_id}});
-            models.Review_Comment.destroy({where: {rc_id : rc_id}}).then(function(ret) {
-                if(ret == 1) {
-                    result.status = 'S';
-                    res.json(result);
-                }
-                else {
-                    res.status(400);
-                    result.status = 'F';
-                    result.reason = 'No review to delete';
-                    res.json(result);
-                }
-            }, function(err) {
-                console.log(err);
-                result.status = 'F';
-                result.reason = err.message;
-                res.json(result);
-            })
-        });
-    });
+  var rc_id = req.params.idx;
+  var rcinfo = req.body;
+  var result = {
+    rc_id: null,
+    status: null,
+    reason: null
+  };
+  models.Review_Comment.destroy({
+    where: {
+      rc_id: rc_id
+    }
+  }).then(function() {
+    result.status = 'S';
+    result.rc_id = rc_id;
+    res.status(200).json(result);
+  }, function(err) {
+    result.status = 'F';
+    result.reason = err.message;
+    res.status(400).json(result);
+  });
+    
 }
 
 module.exports = router;
-
