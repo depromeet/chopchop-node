2a9615639eacb39b58057c64562987bc0ffc2184
Haeseong Jeon <seong0428@gmail.com>
2017-07-10 19:48:18 +0900

    update passport > - move code to /passport/passport.js

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-
-const passport      = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const models        = require('./models');
+var passport     = require('./passport/passport');
 
 // import routes/*.js
 var index           = require('./routes/index');
@@ -32,38 +29,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    models.User.findOne({where: {user_email: username}})
-      .then((user) => {
-        if (!user) {
-          return done(null, false, { message: '존재하지 않는 아이디입니다' }); // 임의 에러 처리
-        }
-        else if(user.dataValues.user_password === password) {             // 검증 성공
-          return done(null, user);
-        }
-        else {
-          return done(null, false, { message: '비밀번호가 틀렸습니다' });  // 임의 에러 처리
-        }
-      })
-      .catch((err) => {
-        return done(err, false);
-      });
-}));
-
-// passport
-passport.serializeUser((user, done) => { // Strategy 성공 시 호출됨
-  console.log('serialize:', user);
-  done(null, user); // 여기의 user가 deserializeUser의 첫 번째 매개변수로 이동
-});
-
-passport.deserializeUser((user, done) => { // 매개변수 user는 serializeUser의 done의 인자 user를 받은 것
-  console.log('deserialize:', user);
-  done(null, user);
-});
-
-
 
 // use session
 app.use(session({
--- passport/passport.js
+++ passport/passport.js
@@ -1,40 +1,37 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const models = require('../models');
+const models = require('./../models');
 
-// const Users = require('./user');
-module.exports = () => {
-  passport.serializeUser((user, done) => { // Strategy 성공 시 호출됨
-    done(null, user); // 여기의 user가 deserializeUser의 첫 번째 매개변수로 이동
-  });
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, (username, password, done) => {
+    models.User.findOne({where: {user_email: username}})
+      .then((user) => {
+        if (!user) {
+          return done(null, false, { message: '존재하지 않는 아이디입니다' }); // 임의 에러 처리
+        }
+        else if(user.dataValues.user_password === password) {             // 검증 성공
+          return done(null, user);
+        }
+        else {
+          return done(null, false, { message: '비밀번호가 틀렸습니다' });  // 임의 에러 처리
+        }
+      })
+      .catch((err) => {
+        return done(err, false);
+      });
+}));
 
-  passport.deserializeUser((user, done) => { // 매개변수 user는 serializeUser의 done의 인자 user를 받은 것
-    done(null, user);
-  });
+// passport
+passport.serializeUser((user, done) => { // Strategy 성공 시 호출됨
+  console.log('serialize:', user);
+  done(null, user); // 여기의 user가 deserializeUser의 첫 번째 매개변수로 이동
+});
 
-  passport.use(new LocalStrategy({ // local 전략을 세움
-    usernameField: 'id',
-    passwordField: 'pw',
-    // session: true, // 세션에 저장 여부
-    // passReqToCallback: false,
-  }, (id, pw, done) => {
-    console.log('id:', id, 'pw:', pw);
-    models.User.findAll({where: {user_email: id}})
-    .then((user) => {
-      console.log('user:', user.dataValues);
-      if (!user)
-        done(null, false, { message: '존재하지 않는 아이디입니다' }); // 임의 에러 처리
-      else if(user.dataValues.user_password == pw) {             // 검증 성공
-        done(null, user);
-      }
-      else {
-        done(null, false, { message: '비밀번호가 틀렸습니다' });  // 임의 에러 처리
-      }
-    })
-    .catch((err) => {
-      done(err, false);
-    });
-  }));
-};
+passport.deserializeUser((user, done) => { // 매개변수 user는 serializeUser의 done의 인자 user를 받은 것
+  console.log('deserialize:', user);
+  done(null, user);
+});
 
-exports.passport = passport;
+module.exports = passport;
